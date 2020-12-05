31f738b5e879ea2c79f73dd1cb65e1e444c49f06
kwak1696 <kwak1696@kakao.com>
2020-12-05 23:31:58 +0900

    Add files via upload

---
+++ LED_TEST/piezo/piezo.ino
@@ -0,0 +1,40 @@
+
+int GroundPin= A0;
+int sensePin= 1;
+int LED=13;
+
+int normalReading= 0;
+int threshold= 0;
+
+int calibrate()
+{
+int n= 5;
+long total=0;
+for (int i=0; i<n;i++)
+ {
+total= total + analogRead(sensePin);
+delay(10);
+}
+return total/n;
+}
+
+void setup()
+{
+pinMode(GroundPin, OUTPUT);
+digitalWrite(GroundPin, LOW);
+pinMode(LED, OUTPUT);
+normalReading= calibrate(); 
+}
+
+void loop()
+{
+int reading= analogRead(sensePin);
+if (reading > normalReading + threshold)
+{
+  digitalWrite(LED, HIGH);
+}
+else
+{
+  digitalWrite(LED, LOW);  
+}
+}
---
+++ LED_TEST/piezo2/piezo2.ino
@@ -0,0 +1,31 @@
+
+
+int GroundPin= A0;
+int sensePin= 1;
+int LED=13;
+int x;
+int normalvalue= 10;
+int threshold= 50;
+
+
+void setup()
+{
+pinMode(LED, OUTPUT);
+x=0;
+}
+
+void loop()
+{
+int value= analogRead(sensePin);
+if (value > normalvalue + threshold)
+{
+  x++;
+  delay(100);
+}
+if(x%2==0)digitalWrite(LED,LOW);
+if(x%2==1)digitalWrite(LED,HIGH);
+
+}
+
+
+
